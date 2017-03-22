47db3b230d8957975e5c94127609fb925519294c
Tony Zhang <422032110@qq.com>
2017-03-22 17:28:54 +0800

    some correct

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -28,10 +28,10 @@ function getDefaultModules() {
         test: /\.css$/, 
         loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}'
       },
-/*      {
+      {
         test: /\.sass/,
         loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
-      },*/
+      },
       {
         test: /\.scss/,
         loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}!sass-loader?outputStyle=expanded'
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,18 +2,19 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //获取图片年相关地数据
-var imageDatas = require('../data/imageDatas.json');
+var imageDatas = require('json!../data/imageDatas.json');
 
 //let yeomanImage = require('../images/yeoman.png');
 
 //利用自执行函数，将图片名信息转为图片URL路径信息
-imageDatas = (function genImageURL(imageDatasArr){
+imageDatas = ((imageDatasArr) => {
 	for(var i=0,len=imageDatasArr.length; i<len ;i++){
-		var singleImageData = imageDatasArr[i];
+		let singleImageData = imageDatasArr[i];
 
-		singleImageData.imageURL=require('../images'+singleImageData.fileName);
+		singleImageData.imageURL=require('../images/'+singleImageData.fileName);
 
 		imageDatasArr[i] = singleImageData;
 	}
@@ -22,9 +23,32 @@ imageDatas = (function genImageURL(imageDatasArr){
 })(imageDatas);
 
 //imageDatas = genImageURL(imageDatas);//文件名转换为文件路径
+var ImgFigure = React.createClass({
+	render: function(){
+		return (
+			<figure>
+				<img src={this.props.data.imageURL}
+					 alt={this.props.data.title}
+				/>
+				<figcaption>
+				<h2>{this.props.data.imageURL}</h2>
+				</figcaption>
+			</figure>
+		)
+	}
+});
+
 
 class AppComponent extends React.Component {
   render() {
+
+  	var controllerUnits = [],
+  		imgFigures = [];
+
+  	imageDatas.forEach((value,index)=>{
+  		imgFigures.push(<ImgFigure data={value} key={'imgFigures'+index}/>)
+  	});
+
     return (
 /*      <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
@@ -33,8 +57,10 @@ class AppComponent extends React.Component {
       </div>*/
       <section className="stage">
       		<section className="img-sec">
+      			{imgFigures}
       		</section>
       		<nav className='controller-nav'>
+      			{controllerUnits}
       		</nav>
       </section>
     );
